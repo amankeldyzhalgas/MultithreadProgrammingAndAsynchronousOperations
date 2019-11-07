7c937e79d7b042cdb200ddb184525d122fa3bb04
amankeldyzhalgas <amankeldy.zhalgas@gmail.com>
2019-11-07 22:52:55 +0600

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Calculate(ulong number)
+        {
+            ulong sum = 0;
+            for (ulong i = 1; i <= number; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Сумма чисел {number} равен {sum}");
+            Console.Write("Введите число(0 для выхода): ");
+        }
+        static async void CalculateAsync(ulong number)
+        {
+            await Task.Run(() => Calculate(number)); 
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число(0 для выхода): ");
+            ulong number = ulong.Parse(Console.ReadLine());
+            while (number != 0)
+            {
+                CalculateAsync(number);
+                number = ulong.Parse(Console.ReadLine());
+            }
+
+            Console.Read();
+        }
+    }
+}
