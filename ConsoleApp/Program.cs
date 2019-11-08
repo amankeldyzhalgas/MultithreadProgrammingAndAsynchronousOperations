ebe70296714aa1a12582511b97c14202e87a92c9
amankeldyzhalgas <amankeldy.zhalgas@gmail.com>
2019-11-08 22:28:50 +0600

    added stylecop

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,39 +1,49 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace ConsoleApp
 {
-    class Program
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class Program. 
+    /// </summary>
+    internal class Program
     {
-        static void Calculate(ulong number, CancellationTokenSource tokenSource)
+        private static void Calculate(ulong number, CancellationTokenSource tokenSource)
         {
             ulong sum = 0;
             for (ulong i = 1; i <= number; i++)
             {
                 if (tokenSource.Token.IsCancellationRequested)
                 {
-                    Console.WriteLine("Операция прервана токеном");
+                    Console.WriteLine("Operation aborted by token.");
                     return;
                 }
 
                 sum += i;
             }
-            Console.WriteLine($"\nСумма чисел {number} равен {sum}\n");
-            Console.Write("Введите число(0 для выхода): ");
-        }
-        static async void CalculateAsync(ulong number, CancellationTokenSource tokenSource)
-        {
-            Console.Write($"Производится вычисление {number} .... ");
-            await Task.Run(() => Calculate(number, tokenSource)); 
+
+            Console.WriteLine($"\nThe sum of numbers from 0 to {number} is equal to {sum}\n");
+            Console.Write("Please enter the number(0 to exit): ");
         }
 
-        static void Main(string[] args)
+        private static async void CalculateAsync(ulong number, CancellationTokenSource tokenSource)
+        {
+            Console.Write($"Processing...The sum of numbers from 0 to {number} is calculating. ");
+            await Task.Run(() => Calculate(number, tokenSource));
+        }
+
+        private static void Main(string[] args)
         {
             CancellationTokenSource tokenSource = new CancellationTokenSource();
 
-            Console.Write("Введите число(0 для выхода): ");
+            Console.Write("Please enter the number(0 to exit): ");
             ulong number = ulong.Parse(Console.ReadLine());
+
             while (number != 0)
             {
                 CalculateAsync(number, tokenSource);
