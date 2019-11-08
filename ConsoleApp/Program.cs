ad5757272c760e733b10d5f708d32b66baaa7819
amankeldyzhalgas <amankeldy.zhalgas@gmail.com>
2019-11-08 17:09:05 +0600

    added cancellationTokenSource

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,39 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Calculate(ulong number)
+        static void Calculate(ulong number, CancellationTokenSource tokenSource)
         {
             ulong sum = 0;
             for (ulong i = 1; i <= number; i++)
             {
+                if (tokenSource.Token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция прервана токеном");
+                    return;
+                }
+
                 sum += i;
             }
-            Console.WriteLine($"Сумма чисел {number} равен {sum}");
+            Console.WriteLine($"\nСумма чисел {number} равен {sum}\n");
             Console.Write("Введите число(0 для выхода): ");
         }
-        static async void CalculateAsync(ulong number)
+        static async void CalculateAsync(ulong number, CancellationTokenSource tokenSource)
         {
-            await Task.Run(() => Calculate(number)); 
+            Console.Write($"Производится вычисление {number} .... ");
+            await Task.Run(() => Calculate(number, tokenSource)); 
         }
 
         static void Main(string[] args)
         {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
             Console.Write("Введите число(0 для выхода): ");
             ulong number = ulong.Parse(Console.ReadLine());
             while (number != 0)
             {
-                CalculateAsync(number);
+                CalculateAsync(number, tokenSource);
                 number = ulong.Parse(Console.ReadLine());
+                tokenSource.Cancel();
+                tokenSource.Dispose();
+                tokenSource = new CancellationTokenSource();
             }
-
-            Console.Read();
         }
     }
 }
